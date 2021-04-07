6fd4627fb0a5ff59e1d9f7116a216c535906ff85
cynthia.anyaeriuba <canyaeriuba@gmail.com>
2021-04-07 20:25:38 +0100

    Challenge 11 - refactored

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -19,16 +19,15 @@ class Oystercard
   def touch_in(entry_station)
     fail 'insufficient balance' if balance < MIN_BALANCE
     
-    @in_journey = true
     @entry_station = entry_station
   end
 
   def in_journey?
-    @in_journey 
+    !@entry_station.nil? # in_journey returns true if entry_station is not nil and vice versa
   end
 
   def touch_out
-    @in_journey = false
+    @entry_station = nil
     deduct(1)
   end
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,59 +1,69 @@
 require 'oystercard'
 
 describe Oystercard do
-  
+  let(:max_balance) { Oystercard::MAX__BALANCE }
+
   it 'is initialised with a balance of 0' do
     expect(subject.balance).to eq 0
   end
 
-  it 'it increases the balance of the card when the top_up method is given a value' do
-    subject.top_up(10)
-    expect(subject.balance).to eq(10)
+  describe '#main' do
+    before do
+      subject.top_up(max_balance)
+    end
+   
+    it 'it increases the balance of the card when the top_up method is given a value' do
+      expect(subject.balance).to eq(max_balance)
+    end
+
+    it "raises an error when balance exceeds £90" do
+      expect{subject.top_up(1)}.to raise_error("Balance cannot exceed £#{max_balance}")
+    end
+
+  # no longer need to have a test for deduct as our test for touch_out covers the logic in the deduct method 
+    # it 'deducts £10 from balance' do
+    #   subject.deduct(10)
+    #   expect(subject.balance).to eq(40)
+    # end
+
+    it 'responds to touch_in' do
+      expect(subject).to respond_to(:touch_in)
+    end
+
+    it  '#in_journey should be true after touch_in' do
+      subject.touch_in("kingscross")    
+      expect(subject).to be_in_journey
+    end
+
+    it '#in_journey should be false after touch_out' do
+  
+      subject.touch_in("kingscross")
+      subject.touch_out
+      expect(subject).not_to be_in_journey
+    end
+
+    it 'deducts minimum fare from balance when journey is complete' do
+  
+      subject.touch_in("kingscross")
+      expect{subject.touch_out}.to change{subject.balance}.by(-1)
+    end
+
+    it 'stores the entry station when touch in' do
+  
+      subject.touch_in("kingscross")
+      expect(subject.entry_station).to eq("kingscross")
+    end
+
+    it 'deletes the entry station at touch out' do
+      subject.touch_in("kingscross")
+      subject.touch_out
+      expect(subject.entry_station).to eq(nil)
+    end
+
   end
-
-  it "raises an error when balance exceeds £90" do
-    subject.top_up(90)
-    expect{subject.top_up(1)}.to raise_error("Balance cannot exceed £#{Oystercard::MAX__BALANCE}")
-  end
-
-# no longer need to have a test for deduct as our test for touch_out covers the logic in the deduct method 
-  # it 'deducts £10 from balance' do
-  #   subject.top_up(50)
-  #   subject.deduct(10)
-  #   expect(subject.balance).to eq(40)
-  # end
-
-  it 'responds to touch_in' do
-    expect(subject).to respond_to(:touch_in)
-  end
-
-  it  '#in_journey should be true after touch_in' do
-    subject.top_up(Oystercard::MAX__BALANCE)
-    subject.touch_in("kingscross")    
-    expect(subject).to be_in_journey
-  end
-
-  it '#in_journey should be false after touch_out' do
-    subject.top_up(Oystercard::MAX__BALANCE)
-    subject.touch_in("kingscross")
-    subject.touch_out
-    expect(subject).not_to be_in_journey
-  end
-
+  
   it 'refuses ride when less than minimum fare' do
     expect {subject.touch_in("kingscross")}.to raise_error('insufficient balance')
-  end
-
-  it 'deducts minimum fare from balance when journey is complete' do
-    subject.top_up(Oystercard::MAX__BALANCE)
-    subject.touch_in("kingscross")
-    expect{subject.touch_out}.to change{subject.balance}.by(-1)
-  end
-
-  it 'stores the entry station when touch in' do
-    subject.top_up(Oystercard::MAX__BALANCE)
-    subject.touch_in("kingscross")
-    expect(subject.entry_station).to eq("kingscross")
   end
 
 end
