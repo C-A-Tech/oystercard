39fc53d28acf4a4baa4fc132a47b32a313a53077
cynthia.anyaeriuba <canyaeriuba@gmail.com>
2021-04-13 06:53:58 +0100

    Challenge 12 - journey history & refactored

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,40 +1,49 @@
 
 class Oystercard 
-  attr_reader :balance, :entry_station
+  attr_reader :entry_station, :exit_station, :journey_history
+  attr_accessor :balance
+  
   MAX__BALANCE = 90
   MIN_BALANCE = 1
 
   def initialize
     @balance = 0
     @in_journey = false
+    @journey_history = []
   end
 
   def top_up(value)
-    fail "Balance cannot exceed £#{MAX__BALANCE}" if (@balance + value) > MAX__BALANCE
-
+    fail "Balance cannot exceed £#{MAX__BALANCE}" if maximun_balance?(value)
     @balance += value
   end
 
 
   def touch_in(entry_station)
     fail 'insufficient balance' if balance < MIN_BALANCE
-    
+    @exit_station = nil
     @entry_station = entry_station
+    
   end
 
   def in_journey?
-    !@entry_station.nil? # in_journey returns true if entry_station is not nil and vice versa
+    !@entry_station.nil? # in_journey returns true if entry_station is not nil & viceversa
   end
 
-  def touch_out
+  def touch_out(exit_station)
+    deduct(MIN_BALANCE)    
+    @exit_station = exit_station
+    @journey_history << {"Entry Station" => @entry_station, "Exit Station" => @exit_station}
     @entry_station = nil
-    deduct(1)
   end
 
   private
 
     def deduct(value)
-    @balance -= value
+      @balance -= value
+    end
+
+    def maximun_balance?(value)
+      (@balance + value) > MAX__BALANCE
     end
   
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,17 +1,42 @@
 require 'oystercard'
 
 describe Oystercard do
-  let(:max_balance) { Oystercard::MAX__BALANCE }
+  # max_balance = Oystercard::MAX__BALANCE
+  let(:max_balance)       { Oystercard::MAX__BALANCE }
+  # entry_station = double("station")
+  let(:entry_station)     { double :station }
 
   it 'is initialised with a balance of 0' do
     expect(subject.balance).to eq 0
   end
 
-  describe '#main' do
-    before do
-      subject.top_up(max_balance)
+  describe '#touch_in' do
+    before {subject.top_up(max_balance)}
+
+    it 'responds to touch_in' do
+      expect(subject).to respond_to(:touch_in)
+    end    
+    
+    it  '#in_journey should be true after touch_in' do
+      subject.touch_in(entry_station)    
+      expect(subject).to be_in_journey
+    end    
+    
+    it 'stores the entry station when touch in' do
+      subject.touch_in(entry_station)
+      expect(subject.entry_station).to eq(entry_station)
+    end      
+    
+    it 'refuses ride when less than minimum fare' do
+      allow(subject).to receive(:balance).and_return 0
+      expect {subject.touch_in("kingscross")}.to raise_error('insufficient balance')
     end
-   
+    
+  end
+
+  describe '#top_up' do
+    before {subject.top_up(max_balance)}
+
     it 'it increases the balance of the card when the top_up method is given a value' do
       expect(subject.balance).to eq(max_balance)
     end
@@ -20,50 +45,42 @@ describe Oystercard do
       expect{subject.top_up(1)}.to raise_error("Balance cannot exceed £#{max_balance}")
     end
 
-  # no longer need to have a test for deduct as our test for touch_out covers the logic in the deduct method 
-    # it 'deducts £10 from balance' do
-    #   subject.deduct(10)
-    #   expect(subject.balance).to eq(40)
-    # end
+  end
 
-    it 'responds to touch_in' do
-      expect(subject).to respond_to(:touch_in)
-    end
-
-    it  '#in_journey should be true after touch_in' do
-      subject.touch_in("kingscross")    
-      expect(subject).to be_in_journey
-    end
+  describe '#touch_out' do
+    before {subject.top_up(max_balance)}
+    before {subject.touch_in("kingscross")}
 
     it '#in_journey should be false after touch_out' do
-  
-      subject.touch_in("kingscross")
-      subject.touch_out
+      subject.touch_out("shoreditch")
       expect(subject).not_to be_in_journey
     end
 
     it 'deducts minimum fare from balance when journey is complete' do
-  
-      subject.touch_in("kingscross")
-      expect{subject.touch_out}.to change{subject.balance}.by(-1)
-    end
-
-    it 'stores the entry station when touch in' do
-  
-      subject.touch_in("kingscross")
-      expect(subject.entry_station).to eq("kingscross")
+      expect{subject.touch_out("shoreditch")}.to change{subject.balance}.by(-1)
     end
 
     it 'deletes the entry station at touch out' do
-      subject.touch_in("kingscross")
-      subject.touch_out
+      subject.touch_out("shoreditch")
       expect(subject.entry_station).to eq(nil)
+    end
+
+    it 'stores the exit station when touch out' do
+  
+      subject.touch_out("shoreditch")
+      expect(subject.exit_station).to eq("shoreditch")
+    end
+
+    it 'tests that journey history is an empty array by default' do
+      expect(subject.journey_history).to eq []
+    end
+
+    it 'returns entire journey history when touch out' do
+      subject.touch_out("shoreditch")
+      expect(subject.journey_history).to eq([{"Entry Station"=>"kingscross", "Exit Station"=>"shoreditch"}])
     end
 
   end
   
-  it 'refuses ride when less than minimum fare' do
-    expect {subject.touch_in("kingscross")}.to raise_error('insufficient balance')
-  end
 
 end
